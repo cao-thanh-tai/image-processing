ad0f8b182cc5e67bef433ebe5115a1d333913393
cao-thanh-tai <shiinahiyori99@gmail.com>
2026-01-18 22:39:44 +0700

    cac ham co ban

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+image = cv2.imread('../data/raw/Apple/Fresh/apple_fresh_001.jpg (1).jpg')
+
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+wight = image.shape[1]
+height = image.shape[0]
+aspect_ratio = wight / height
+plt.figure(figsize=(5*aspect_ratio, 5))
+plt.title("Grayscale Image")
+cv2.imshow(image_gray)
+plt.show()
+
